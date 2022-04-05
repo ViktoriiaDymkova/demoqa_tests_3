de90e779b6458bb1e8b7af2d8cf6aa379568c15e
user <stel-vd@yandex.ru>
2022-04-05 22:27:38 +0300

    small refactor & added logger slf4j

--- src/test/java/guru/qa/demoqa_tests.java
+++ src/test/java/guru/qa/demoqa_tests.java
@@ -1,12 +1,24 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-
 public class demoqa_tests {
-    @Test
-    void fillFormTest(){
-        open ("https://demoqa.com/automation-practice-form");
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
     }
+
+    @Test
+    void fillFormTests() {
+Selenide.open("/automation-practice-form");
+        Selenide.zoom(0.65);
+
+Selenide.$("[id=firstName").setValue("vika");
+    }
+
 }
