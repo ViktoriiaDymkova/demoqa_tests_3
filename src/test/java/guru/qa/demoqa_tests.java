4e306f622cb27601b247002232e270039e0ad0dd
user <stel-vd@yandex.ru>
2022-04-05 18:46:55 +0300

    small refactor & added logger slf4j

--- src/test/java/guru/qa/demoqa_tests.java
+++ src/test/java/guru/qa/demoqa_tests.java
@@ -2,5 +2,11 @@ package guru.qa;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class demoqa_tests {
+    @Test
+    void fillFormTest(){
+        open ("https://demoqa.com/automation-practice-form");
+    }
 }
