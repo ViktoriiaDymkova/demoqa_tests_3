f76aec99e88a5261576749dc4cc797a9adb2c59b
user <stel-vd@yandex.ru>
2022-04-16 21:35:50 +0300

    base with check

--- src/test/java/guru/qa/RegistrationFormTests.java
+++ src/test/java/guru/qa/RegistrationFormTests.java
@@ -4,23 +4,28 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class Demoqa_tests {
+public class RegistrationFormTests {
+
     @BeforeAll
     static void beforeAll() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-
     }
+
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        //executeJavaScript("$('footer').remote()");
+        //executeJavaScript("$('#fixe').remote()");
         zoom(0.65);
 
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         $("[id=firstName]").setValue("vika");
         $("[id=lastName]").setValue("kom");
         $("[id=userEmail]").setValue("vika@kom.com");
@@ -29,7 +34,7 @@ public class Demoqa_tests {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1991");
-        $(".react-datepicker__day--011").click();
+        $(".react-datepicker__day--011:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $("#hobbiesWrapper").$(byText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("bruss.jpeg");
@@ -39,7 +44,9 @@ public class Demoqa_tests {
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
-
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+       $(".table-responsive").shouldHave(text("vika kom"));
+       $(".table-responsive").$(byText("Student Name"))
+                .parent().shouldHave(text("vika kom"));
     }
-
 }
